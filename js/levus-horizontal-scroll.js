cfbe7d8cb550caeee830100f0f360593b1aa80bc
duncan <lvivduncan@gmail.com>
2020-08-02 22:17:59 +0300

    universal function

--- js/levus-horizontal-scroll.js
+++ js/levus-horizontal-scroll.js
@@ -1,72 +1,120 @@
-class LevusHorizontalScroll {
-    constructor(scroll) {
-        // scroll
-        this.scroll = document.querySelector(scroll);
+// 2-08-2020
+{
+    // scroll
+    const scroll = document.querySelectorAll('.levus-horizontal-scroll');
+
+    // if exists one scroll
+    if (scroll.length === 1) {
+
         // wrapper
-        this.ul = this.scroll.querySelector('.levus-horizontal-scroll ul');
+        const ul = scroll[0].querySelector('.levus-horizontal-scroll ul');
         // elements
-        this.li = this.ul.querySelectorAll('li');
-    }
+        let li = ul.querySelectorAll('li');
 
-    clone() {
-        // cloned and append elements
-        this.li.forEach(element => this.ul.append(element.cloneNode(true)));
-        // new nodelist
-        this.li = this.scroll.querySelectorAll('.levus-horizontal-scroll li');
-    }
+        // if less than 5, cloned 
+        if (li.length <= 5) {
+            // cloned and append elements
+            li.forEach(element => ul.append(element.cloneNode(true)));
+            // new nodelist
+            li = scroll[0].querySelectorAll('.levus-horizontal-scroll li');
+        }
 
-    translate() {
-        this.ul.style.left = `-25%`;
-    }
+        ul.style.left = `-25%`;
 
-    buttons() {
-        // create button
-        const left = document.createElement('span');
-        left.setAttribute('class', 'left');
-        this.scroll.prepend(left);
+        scroll[0].innerHTML += '<span class="left"></span><span class="right"></span>';
 
-        // create button
-        const right = document.createElement('span');
-        right.setAttribute('class', 'right');
-        this.scroll.prepend(right);
-
-        this.scroll.addEventListener('click', (e) => {
+        scroll[0].addEventListener('click', (e) => {
+            const ul = scroll[0].querySelector('.levus-horizontal-scroll ul');
             if (e.target.className == 'left') {
                 // move last element
-                this.ul.prepend(this.ul.lastElementChild);
+                const last = ul.lastElementChild;
+                ul.prepend(last);
                 // destroy transition
-                this.ul.style.transition = '';
+                ul.style.transition = '';
                 // animation
-                this.ul.style.transform = 'translateX(-25%)';
+                ul.style.transform = 'translateX(-25%)';
                 setTimeout(() => {
-                    this.ul.style.transform = 'translateX(0)';
-                    this.ul.style.transition = '.5s';
+                    ul.style.transform = 'translateX(0)';
+                    ul.style.transition = '.5s';
                 }, 50);
             }
         });
 
-        this.scroll.addEventListener('click', (e) => {
+        scroll[0].addEventListener('click', (e) => {
+            const ul = scroll[0].querySelector('.levus-horizontal-scroll ul');
             if (e.target.className == 'right') {
                 // move first element
-                this.ul.append(this.ul.firstElementChild);
+                const first = ul.firstElementChild;
+                ul.append(first);
                 // destroy transition
-                this.ul.style.transition = '';
+                ul.style.transition = '';
                 // animation
-                this.ul.style.transform = 'translateX(25%)';
+                ul.style.transform = 'translateX(25%)';
                 setTimeout(() => {
-                    this.ul.style.transform = 'translateX(0)';
-                    this.ul.style.transition = '.5s';
+                    ul.style.transform = 'translateX(0)';
+                    ul.style.transition = '.5s';
                 }, 50);
             }
         });
     }
 
-    init() {
-        // if less than 5, cloned 
-        this.li.length <= 5 && this.clone();
-        this.translate();
-        this.buttons();
+    // if exists more than one scroll
+    if (scroll.length > 1) {
+
+        scroll.forEach(item => {
+            const ul = item.querySelector('.levus-horizontal-scroll ul');
+            // elements
+            let li = ul.querySelectorAll('li');
+
+            // if less than 5, cloned 
+            if (li.length <= 5) {
+                // cloned and append elements
+                li.forEach(element => ul.append(element.cloneNode(true)));
+                // new nodelist
+                li = item.querySelectorAll('.levus-horizontal-scroll li');
+            }
+
+            ul.style.left = `-25%`;
+
+
+            item.innerHTML += '<span class="left"></span><span class="right"></span>';
+
+            item.addEventListener('click', (e) => {
+                const ul = item.querySelector('.levus-horizontal-scroll ul');
+                if (e.target.className == 'left') {
+                    // move last element
+                    const last = ul.lastElementChild;
+                    ul.prepend(last);
+                    // destroy transition
+                    ul.style.transition = '';
+                    // animation
+                    ul.style.transform = 'translateX(-25%)';
+                    setTimeout(() => {
+                        ul.style.transform = 'translateX(0)';
+                        ul.style.transition = '.5s';
+                    }, 50);
+                }
+            });
+
+            item.addEventListener('click', (e) => {
+                const ul = item.querySelector('.levus-horizontal-scroll ul');
+                if (e.target.className == 'right') {
+                    // move first element
+                    const first = ul.firstElementChild;
+                    ul.append(first);
+                    // destroy transition
+                    ul.style.transition = '';
+                    // animation
+                    ul.style.transform = 'translateX(25%)';
+                    setTimeout(() => {
+                        ul.style.transform = 'translateX(0)';
+                        ul.style.transition = '.5s';
+                    }, 50);
+                }
+            });
+
+
+        });
+
     }
 }
-
-document.querySelector('.levus-horizontal-scroll') && new LevusHorizontalScroll('.levus-horizontal-scroll').init();
---
+++ js/levus-horizontal-scroll.oop.js
@@ -0,0 +1,72 @@
+class LevusHorizontalScroll {
+    constructor(scroll) {
+        // scroll
+        this.scroll = document.querySelector(scroll);
+        // wrapper
+        this.ul = this.scroll.querySelector('.levus-horizontal-scroll ul');
+        // elements
+        this.li = this.ul.querySelectorAll('li');
+    }
+
+    clone() {
+        // cloned and append elements
+        this.li.forEach(element => this.ul.append(element.cloneNode(true)));
+        // new nodelist
+        this.li = this.scroll.querySelectorAll('.levus-horizontal-scroll li');
+    }
+
+    translate() {
+        this.ul.style.left = `-25%`;
+    }
+
+    buttons() {
+        // create button
+        const left = document.createElement('span');
+        left.setAttribute('class', 'left');
+        this.scroll.prepend(left);
+
+        // create button
+        const right = document.createElement('span');
+        right.setAttribute('class', 'right');
+        this.scroll.prepend(right);
+
+        this.scroll.addEventListener('click', (e) => {
+            if (e.target.className == 'left') {
+                // move last element
+                this.ul.prepend(this.ul.lastElementChild);
+                // destroy transition
+                this.ul.style.transition = '';
+                // animation
+                this.ul.style.transform = 'translateX(-25%)';
+                setTimeout(() => {
+                    this.ul.style.transform = 'translateX(0)';
+                    this.ul.style.transition = '.5s';
+                }, 50);
+            }
+        });
+
+        this.scroll.addEventListener('click', (e) => {
+            if (e.target.className == 'right') {
+                // move first element
+                this.ul.append(this.ul.firstElementChild);
+                // destroy transition
+                this.ul.style.transition = '';
+                // animation
+                this.ul.style.transform = 'translateX(25%)';
+                setTimeout(() => {
+                    this.ul.style.transform = 'translateX(0)';
+                    this.ul.style.transition = '.5s';
+                }, 50);
+            }
+        });
+    }
+
+    init() {
+        // if less than 5, cloned 
+        this.li.length <= 5 && this.clone();
+        this.translate();
+        this.buttons();
+    }
+}
+
+document.querySelector('.levus-horizontal-scroll') && new LevusHorizontalScroll('.levus-horizontal-scroll').init();
