bc446a07a4b22b9c68e5391776872b8aba194b97
lvivduncan <lvivduncan@gmail.com>
2020-10-08 17:57:03 +0300

    grid experiment

--- js/levus-horizontal-scroll.js
+++ js/levus-horizontal-scroll.js
@@ -1,9 +1,9 @@
-// 4-08-2020
+// 8-10-2020
 {
     // scroll
     const scroll = document.querySelectorAll('.levus-horizontal-scroll');
 
-    // if exists one scroll
+    /* // if exists one scroll
     if (scroll.length === 1) {
 
         // wrapper
@@ -52,10 +52,10 @@
                 }, 50);
             }
         });
-    }
+    } */
 
     // if exists more than one scroll
-    if (scroll.length > 1) {
+/*     if (scroll.length > 1) {
 
         scroll.forEach(item => {
             const ul = item.querySelector('.levus-horizontal-scroll ul');
@@ -63,12 +63,14 @@
             let li = ul.querySelectorAll('li');
 
             // if less than 4, cloned 
-            if (li.length < 4) {
+            if (li.length <= 4) {
                 // cloned and append elements
                 li.forEach(element => ul.append(element.cloneNode(true)));
                 // new nodelist
                 li = item.querySelectorAll('.levus-horizontal-scroll li');
             }
+
+            console.log(li.length)
 
             item.innerHTML += '<span class="left"></span><span class="right"></span>';
 
@@ -104,5 +106,54 @@
                 }
             });
         });
-    }
+    } */
+
+    scroll.forEach(item => {
+        const ul = item.querySelector('.levus-horizontal-scroll ul');
+        // elements
+        let li = ul.querySelectorAll('li');
+
+        // if less than 4, cloned 
+        if (li.length <= 4) {
+            // cloned and append elements
+            li.forEach(element => ul.append(element.cloneNode(true)));
+            // new nodelist
+            li = item.querySelectorAll('.levus-horizontal-scroll li');
+        }
+
+        item.innerHTML += '<span class="left"></span><span class="right"></span>';
+
+        item.addEventListener('click', (e) => {
+            const ul = item.querySelector('.levus-horizontal-scroll ul');
+            if (e.target.className == 'left') {
+                // move last element
+                const last = ul.lastElementChild;
+                ul.prepend(last);
+                // destroy transition
+                ul.style.transition = 'none';
+                ul.classList.add('to-right');
+                setTimeout(() => {
+                    ul.classList.remove('to-right');
+                    ul.style.transition = '.5s';
+                }, 50);
+            }
+        });
+
+        item.addEventListener('click', (e) => {
+            const ul = item.querySelector('.levus-horizontal-scroll ul');
+            if (e.target.className == 'right') {
+                // move first element
+                const first = ul.firstElementChild;
+                ul.append(first);
+                // destroy transition
+                ul.style.transition = 'none';
+                ul.classList.add('to-left');
+                setTimeout(() => {
+                    ul.classList.remove('to-left');
+                    ul.style.transition = '.5s';
+                }, 50);
+            }
+        });
+    });
+
 }
