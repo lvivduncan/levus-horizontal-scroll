7a6f61d42454a30de3dcd8e4c48caa5cc91893a3
lvivduncan <lvivduncan@gmail.com>
2020-12-02 23:03:06 +0200

    add function.ie5.js

---
+++ js/levus-horizontal-scroll.funciton.ie5.js
@@ -0,0 +1,50 @@
+// 2-12-2020
+"use strict";
+function levusHorizontalScroll(selector) {
+  var scroll = document.querySelectorAll(selector);
+  scroll.forEach(function (item) {
+    var ul = item.querySelector('ul');
+    var li = ul.querySelectorAll('li');
+
+    if (li.length <= 4) {
+      li.forEach(function (element) {
+        return ul.append(element.cloneNode(true));
+      });
+      li = item.querySelectorAll('li');
+    }
+
+    item.innerHTML += '<span class="left"></span><span class="right"></span>';
+    item.addEventListener('click', function (e) {
+      var ul = item.querySelector('ul');
+
+      if (e.target.className == 'left') {
+        var last = ul.lastElementChild;
+        ul.prepend(last);
+
+        ul.style.transition = 'none';
+        ul.classList.add('to-right');
+        setTimeout(function () {
+          ul.classList.remove('to-right');
+          ul.style.transition = '.5s';
+        }, 50);
+      }
+    });
+    item.addEventListener('click', function (e) {
+      var ul = item.querySelector('ul');
+
+      if (e.target.className == 'right') {
+        var first = ul.firstElementChild;
+        ul.append(first);
+
+        ul.style.transition = 'none';
+        ul.classList.add('to-left');
+        setTimeout(function () {
+          ul.classList.remove('to-left');
+          ul.style.transition = '.5s';
+        }, 50);
+      }
+    });
+  });
+}
+
+levusHorizontalScroll('.levus-horizontal-scroll');
